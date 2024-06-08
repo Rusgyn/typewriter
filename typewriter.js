7b9d9c76ee975d4e809ccb78394cce40e996aa81
rusgyn <rusgynmagsanay@gmail.com>
2024-06-08 03:30:35 -0400

    Define typewriter().

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+// Our goal is to animate the sentence, revealing one character at a time. This would make it look as though it is being typed in by someone.
+
+const typeWriter = function(sentence) {
+  console.log(sentence);
+};
+
+typeWriter("hello there from lighthouse labs");
