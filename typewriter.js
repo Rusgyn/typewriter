f8c462cf10d6aa31b0ba36afbc5b7cdcd05247a6
rusgyn <rusgynmagsanay@gmail.com>
2024-06-08 03:39:20 -0400

    Implement the function using setTimeOut method to animate the input

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,15 @@
 // Our goal is to animate the sentence, revealing one character at a time. This would make it look as though it is being typed in by someone.
 
 const typeWriter = function(sentence) {
-  console.log(sentence);
+  
+  let time = 0;
+
+  for (let char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, time += 100);
+  }
+
 };
 
-typeWriter("hello there from lighthouse labs");
+typeWriter("Animated sentence using setTimeOut method");
